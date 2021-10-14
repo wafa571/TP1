4d8aca0cc33d359b9c869e123b7b0be89927cbb2
wafa571 <wafa.youssef.tn@gmail.com>
2021-10-14 09:25:31 +0100

    hello world

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("MainActivity", "INFORMATION");
+        Log.e("MainActivity", "ERROR");
+        Log.w("MainActivity", "WARNING");
+
+
+    }
+}
